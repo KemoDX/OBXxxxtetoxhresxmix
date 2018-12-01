bd365c28bf69cac58c7006158ba101be8d54af6e
KemoDX <43991891+KemoDX@users.noreply.github.com>
2018-12-01 07:16:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1026,6 +1026,44 @@ return message.reply("**:white_check_mark: .. تم فك الميوت عن الش
 
 });
 
+ kboosh.on('message', message => {
+    if (message.content.split(' ')[0] == '^send')
+       message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+
+           member.send( " ** " + message.content.substr(3));
+                                                      message.delete();
+            
+                                                    });
+            
+                                                  });
+   kboosh.on("message", message => {
+       var prefix = "^";
+ 
+             var args = message.content.substring(prefix.length).split(" ");
+                if (message.content.startsWith(prefix + "b")) {
+                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+                          if (!args[1]) {
+                            
+                                 let embed3 = new Discord.RichEmbed()
+                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
+                                       .setColor("#FF00FF")
+                                          message.channel.sendEmbed(embed3);
+                            
+                                        } else {
+
+                            
+                                           let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
+                                                                .setColor("#99999")
+                               
+                                                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
+});
 
 client.on('message', message => {
 
@@ -1055,6 +1093,8 @@ client.on('message', message => {
                 
          
        
+});
+
 });
 
 client.on('message', message => {
